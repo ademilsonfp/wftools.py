30c5e29ba8ffbfeb3227fea484b7f5139e019fe2
ademilsonfp <ademilsonfp@gmail.com>
2013-11-11 21:36:50 -0200

    jade prototyping supporting multiline strings as variable

--- jade.py
+++ jade.py
@@ -1,8 +1,16 @@
 # coding: utf-8
 
-import os, tools, paths
+import os, re, tools, paths
 
 COMMAND = 'jade -P %s -o %s'
+
+def _inline_json(json):
+  def repl(match):
+    return match.group().replace('\n', '\\x0a')
+  json = re.sub(r'\'(|([^\'\\]+|\\[^\']|\\\')*)\'|"(|([^"\\]+|\\[^"]|\\")*)"',
+      repl, json)
+  json = json.replace('\n', ' ').replace('"', '\\"')
+  return json
 
 def _build_update(updated):
   pre_size = len(paths.jade()) + 1
@@ -13,7 +21,7 @@ def _build_update(updated):
     ext_start = path.rfind('.')
     ext = path[ext_start:]
     if '.json' == ext:
-      continue
+      path = path[:ext_start] + '.jade'
     dst_path = paths.html(path[:pre_size])
     json_path = path[:ext_start] + '.json'
     if not os.path.exists(path) and os.path.exists(dst_path):
@@ -22,8 +30,8 @@ def _build_update(updated):
     else:
       suf = html_path
       if os.path.exists(json_path):
-        json = open(json_path).read().replace('"', '\\"').replace('\n', ' ')
-        suf += ' -O "%s"' % json
+        json = open(json_path).read()
+        suf += ' -O "%s"' % _inline_json(json)
       code = os.system(COMMAND % (path, suf))
       if 0 != code:
         raise Exception('Error %d while building jade file %s' % (code, path))
