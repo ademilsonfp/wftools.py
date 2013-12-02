49e45ffdc1905c9aca2a6b0699cc68991a9079e2
ademilsonfp <ademilsonfp@gmail.com>
2013-12-02 15:06:35 -0200

    less cleanup

--- less.py
+++ less.py
@@ -1,28 +1,27 @@
 # coding: utf-8
 
-import os, tools, paths
+'''
+Tools to build LESS styles.
+'''
 
-COMMAND = 'lessc --verbose --strict-imports %s %s'
+import os, tools
 
-def _build(updated):
-  pre_size = len(paths.less()) + 1
-  for path in updated:
-    dst_path = paths.css(path[pre_size:path.rfind('.')] + '.css')
-    if not os.path.exists(path) and os.path.exists(dst_path):
-      os.remove(dst_path)
-      print '%s removed' % dst_path
-    else:
-      code = os.system(COMMAND % (path, dst_path))
-      if 0 != code:
-        raise Exception('Error %d while building less file %s' % (code, path))
+'''
+LESS command or path.
+'''
+CMD = 'lessc'
 
-def build(*src):
-  if 1 > len(src):
-    src = [paths.less('*.less')]
-  tools.watch(src, _build, paths.cache('less_build'))
+def build(src_path, path):
+  '''
+  Builds a single LESS file to specified path.
+  '''
+  if not os.path.exists(src_path):
+    if os.path.exists(path):
+      os.unlink(path)
+      print '%s removed' % tools.subpath(path)
+    return
 
-def rebuild(*src):
-  cache = paths.cache('less_build')
-  if os.path.exists(cache):
-    os.remove(cache)
-  build(*src)
+  cmd = [CMD, '--verbose', '--strict-imports', src_path, path]
+  code = os.system(' '.join(cmd))
+  if 0 != code:
+    raise Exception('Error %d while building LESS file %s' % (code, src_path))
